31e6b64f89c6393d4ffa461b9168d603c645ffd9
Michael <mikedigregorio135@gmail.com>
2020-02-03 15:46:21 -0500

    all of the package dependencies build + an instruction set on how to build all of the dependencies

--- hrp_model/build/build_hrp.sh
+++ hrp_model/build/build_hrp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 pip install -U pip
-python3 -m pip install scipy numpy matplotlib pandas datetime cvxopt alpha_vantage
+sudo apt-get install libgmp-dev libmpfr-dev libmpc-dev
+python3 -m pip install scipy numpy matplotlib pandas datetime cvxopt alpha_vantage cython scikit-learn bottleneck munkres fabia ffn config gmpy
+sudo apt install python-rpy2 python-gmpy
+sudo apt install python3-tk
--- hrp_model/src/main.py
+++ hrp_model/src/main.py
@@ -18,7 +18,15 @@
 # distribute the allocation through recursive bisection based on 
 # cluster covariance.
 
-
+import sys
+#to compile this you need to have biclustlib installed onto your machine 
+#the link to their github is: https://github.com/padilha/biclustlib
+#get that, set up a virtualenv using the instructions in the create_ssmif_env.sh script 
+# (it just prints what you should do)
+#then, run the build_hrp.sh script which will install the dependencies needed for biclustlib
+#then you should run python setup.py install in the root directory of the biclustlib package
+#after that, you should be able to run this package
+sys.path.insert(0, "/home/mycicle/ssmif/biclustlib")
 import scipy.cluster.hierarchy as sch
 import numpy as np
 import pandas as pd
@@ -29,13 +37,20 @@ from cvxopt import blas, solvers
 from alpha_vantage.timeseries import TimeSeries
 import ffn
 import config
-
+from biclustlib.algorithms import BiCorrelationClusteringAlgorithm
 #up first, correlation clustering functions
-# lets see if we can use fabia biclustering
-if "__name__" == __main__:
+# We will use BCCA Bi-Correlation Clustering Algorithm
+# for this implementation
+# We get the package from this source
+#  Padilha, V. A. & Campello, R. J. G. B. (2017).
+#  A systematic comparative evaluation of biclustering techniques. BMC Bioinformatics, 18(1):55.
+
+def correlationCluster(data):
+    pass
+if __name__ == "__main__":
     data = dict()
 
-    clusters = correlationCluster(data)
-    qdiag = quasiDiagonalization(clusters)
-    allocations = recBisection(qdiag)
-    print(allocations)
+    #clusters = correlationCluster(data)
+    #qdiag = quasiDiagonalization(clusters)
+    #allocations = recBisection(qdiag)
+    #print(allocations)
