pretty sure the varaince covariance matrix is correct  it matches up to 10e-6 or -7 precision with traditionally calcualted variance. check out getCovarianceMatrix2 to see the procedurally calculated variance   i havent checked out the efficacy of the covariance parts of the matrix with financial data but it works with simple dummy data

src/calcImpliedEquilibriumReturns.py
@@ -22,83 +22,85 @@ def getDailyReturns(equity_df, basis="Adj Close"):
 
 # def getCovarianceMatrixOfReturns():
 #def getCovarianceMatrixOfReturns(lists):
-# def getCovarianceMatrixOfReturns(equities, start, end, basis="Adj Close"):
-# #This implementation is based off of this example https://stattrek.com/matrix-algebra/covariance-matrix.aspx
-
-#     # basis_columns = []
-    
-#     # for index, equity in enumerate(equities):
-#     #     print(equity)
-#     #     basis_columns.append(list(getTickerStats(equity, start=start, end=end)[0][basis].values))
-
-#     # for index, col in enumerate(basis_columns):
-#     #     basis_columns[index] = getPercentChange(col)
-
-#     equity_df = getTickerStats(equities, start=start, end=end)
-
-#     basis_columns = getDailyReturns(equity_df)
-#     # for scores in lists:
-#     #     basis_columns.append(scores)
-# ########################################TESTING CONTENT############################################
-# #each sub list represents a different "student's test scores" in math english and art
-
-#     # basis_columns = [[90, 60, 90],
-#     #                  [90, 90, 30],
-#     #                  [60, 60, 60],
-#     #                  [60, 60, 90],
-#     #                  [30,30,30]]
-
-#     #basis_columns = [[90,90,60,60,30], [60,90,60,60,30], [90,30,60,90,30]]
-
-# ########################################TESTING CONTENT#############################################
-
-#     X = np.array(basis_columns).T.tolist()
-
-#     print("\n#############################\n")
-#     print("Raw Data:")
-#     print(X)
-    
-#     #time for the math
-#     #first we will transform the raw data matrix X into deviation scores for matrix x
-#     #x = X - 11'X (1.0/n) where 
-#     # X is an n x k matrix of returns, x is an n x k matrix of deviation scores, and 1 is an n x 1 column 
-#     # vector of ones
-
-#     n = len(X)
-#     print(n)
-#     #one = 11'
-#     one = np.ones((n,n))
-
-#     #x = X - (1.0/n) * 11'X
-#     x = np.subtract(X, np.multiply( (1/n), np.matmul(one, X) ))
-
-#     #Then we will compute x'x, the k x k deviation sums of squares and cross products matrix for x
-#     dev_ss_cp = np.matmul(x.transpose(), x)
-    
-#     #then we will divide each term in the deviation sum of squares and cross products by n to create the variance 
-#     #covariance matrix
-#     V  = np.multiply((1/n), dev_ss_cp)
-
-#     print("\n#############################\n")
-#     print("Variance-Covariance Matrix:")
-#     print(V)
-#     print("\n#############################\n")
-
-#     #V should be a k x k (num assets x num assets) variance covariance matrix (aka covariance matrix) for the 
-#     #columns input into the function
-#     #The diagonal of V is the variance of returns for each equity and the covariance is represented by the 
-#     #off diagonal elements of V
-#     sns.heatmap(V)
-#     pt.show()
-#     return V
-
 def getCovarianceMatrixOfReturns(equities, start, end, basis="Adj Close"):
-    basis_df = pdr.DataReader(["GOOG"], 'yahoo', start=start, end=end)[basis]
+#This implementation is based off of this example https://stattrek.com/matrix-algebra/covariance-matrix.aspx
+
+    # basis_columns = []
+    
+    # for index, equity in enumerate(equities):
+    #     print(equity)
+    #     basis_columns.append(list(getTickerStats(equity, start=start, end=end)[0][basis].values))
+
+    # for index, col in enumerate(basis_columns):
+    #     basis_columns[index] = getPercentChange(col)
+
+    equity_df = getTickerStats(equities, start=start, end=end)
+
+    basis_columns = getDailyReturns(equity_df)
+    # for scores in lists:
+    #     basis_columns.append(scores)
+########################################TESTING CONTENT############################################
+#each sub list represents a different "student's test scores" in math english and art
+
+    # basis_columns = [[90, 60, 90],
+    #                  [90, 90, 30],
+    #                  [60, 60, 60],
+    #                  [60, 60, 90],
+    #                  [30,30,30]]
+
+    # basis_columns = [[90,90,60,60,30], [60,90,60,60,30], [90,30,60,90,30]]
+
+########################################TESTING CONTENT#############################################
+
+    X = np.array(basis_columns).T.tolist()
+
+    print("\n#############################\n")
+    print("Raw Data:")
+    print(X)
+    
+    #time for the math
+    #first we will transform the raw data matrix X into deviation scores for matrix x
+    #x = X - 11'X (1.0/n) where 
+    # X is an n x k matrix of returns, x is an n x k matrix of deviation scores, and 1 is an n x 1 column 
+    # vector of ones
+
+    n = len(X)
+    print(n)
+    #one = 11'
+    one = np.ones((n,n))
+
+    #x = X - (1.0/n) * 11'X
+    x = np.subtract(X, np.multiply( (1/n), np.matmul(one, X) ))
+
+    #Then we will compute x'x, the k x k deviation sums of squares and cross products matrix for x
+    dev_ss_cp = np.matmul(x.transpose(), x)
+    
+    #then we will divide each term in the deviation sum of squares and cross products by n to create the variance 
+    #covariance matrix
+    V  = np.multiply((1/n), dev_ss_cp)
+
+    print("\n#############################\n")
+    print("Variance-Covariance Matrix:")
+    print(V)
+    print("\n#############################\n")
+
+    #V should be a k x k (num assets x num assets) variance covariance matrix (aka covariance matrix) for the 
+    #columns input into the function
+    #The diagonal of V is the variance of returns for each equity and the covariance is represented by the 
+    #off diagonal elements of V
+    # sns.heatmap(V)
+    # pt.show()
+    return V
+
+def getCovarianceMatrixOfReturns2(equities, start, end, basis="Adj Close"):
+    basis_df = pdr.DataReader(["STZ"], 'yahoo', start=start, end=end)[basis]
     #V = np.multiply(basis_df.pct_change().dropna().cov(), (len(basis_df) - 1) / len(basis_df))
     #print(V)
-    variance = (basis_df.pct_change()["GOOG"].mean() - basis_df.pct_change()["GOOG"])**2 / len(basis_df["GOOG"])
-    print(variance)
-    return V
+    basis_col = basis_df["STZ"]
+    change = basis_col.pct_change()
+    variance = (change.std() * (len(change)-1)/(len(change)))**2
+    print(variance)    
+    #return V
 
 def getTickerStats(tickers, start, end):
         equity_df = []
@@ -118,6 +120,7 @@ if __name__ == "__main__":
     #print(getCovarianceMatrixOfReturns(equity_df, basis="Adj Close"))
     # getCovarianceMatrixOfReturns([[90, 60, 90], [90,90,30], [60,60,60], [60,60,90], [30,30,30]])
     getCovarianceMatrixOfReturns(equities, start=start, end=end, basis="Adj Close" )
+    getCovarianceMatrixOfReturns2(equities, start=start, end=end, basis="Adj Close" )
     #print(getDailyReturns(getTickerStats(["HEDJ"], start=start, end=end)))
     #thing = getTickerStats(["HEDJ"], start=start, end=end)
     #print(getDailyReturns(thing))